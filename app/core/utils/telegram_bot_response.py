d5a2ee51b9b34a85f668a370df0e7ad6da831456
fazliddin <ibragimov.asus15@gmail.com>
2025-02-13 18:28:20 +0500

    user id and json encoding added to telegram_bot_response.py

--- app/core/utils/telegram_bot_response.py
+++ app/core/utils/telegram_bot_response.py
@@ -12,7 +12,7 @@ async def send_order_notification_to_telegram(order: Order):
 	text = (
 		f"🛒 *New Order Placed!* \n\n"
 		f"📦 *Order ID:* `{order.id}`\n"
-		f"👤 *User ID:* `{order.user_id}`\n"
+		f"👤 *User ID:* #ID{order.user_id}\n"
 		f"💰 *Total Amount:* `${order.total_amount:.2f}`\n"
 		f"📦 *Total Items:* `{order.total_quantity}`\n"
 		f"🕒 *Ordered At:* `{order.created_at.strftime('%Y-%m-%d %H:%M:%S')}`\n"
@@ -27,4 +27,5 @@ async def send_order_notification_to_telegram(order: Order):
 
 	async with httpx.AsyncClient() as client:
 		response = await client.post(f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage', json=payload)
-		return {"status": response.status_code, "message": response.text}
+		from json import loads
+		return {"status": response.status_code, "message": loads(response.text)}
